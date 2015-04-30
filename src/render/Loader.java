b84481b51db8bf49c26b8cded2f6b4a85de9f858
dantetam <dante.tam1@gmail.com>
2015-04-29 17:11:44 -0700

    Mipmapping/texture optimization

--- src/render/Loader.java
+++ src/render/Loader.java
@@ -11,6 +11,7 @@ import models.RawModel;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -73,6 +74,9 @@ public class Loader {
 		try 
 		{
 			texture = TextureLoader.getTexture("PNG",new FileInputStream("res/"+fileName+".png"));
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
 		} 
 		catch (Exception e) {e.printStackTrace();}
 		int textureID = texture.getTextureID();
